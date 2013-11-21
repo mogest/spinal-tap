5b51700e7db8e6fa8de4aa0b80595cfd1ddad58b
Roger Nesbitt <roger@seriousorange.com>
2013-11-21 21:28:09 +1300

    better design, validations, events

---
+++ README.md
@@ -0,0 +1,62 @@
+Spinal Tap
+==========
+
+Yet another simple Javascript model framework.  If you use Rails, you'll feel right at home with this one.
+
+Note this is all very new and probably shouldn't be used in production until it has tests.
+
+An example of how to use it
+---------------------------
+
+I'll have a better/more generic example here eventually, but this is a dump of what I've been using to test:
+
+````javascript
+  Prospect = new SpinalTap.Model({url: "http://localhost:3000/prospects"});
+  Prospect.extend({
+    events: {
+      "initialization": function(e)       { console.log("init"); },
+      "beforeSave":     function(e, opts) { console.log("before save", opts); },
+      "afterSave":      function(e, data) { console.log("after save", data); },
+    },
+
+    attributesToWire: function(attributes) {
+      return {prospect: attributes};
+    },
+
+    // id is actually called ‘token’ for prospects
+    getID: function() {
+      return this.a.token;
+    },
+
+    // Only allow some attributes to be sent to the server
+    getSaveableAttributes: function() {
+      var data = this.getChangedAttributes(),
+          acceptedKeys = ['first_name', 'last_name', 'selected_network_area_id'],
+          result = {};
+
+      _.each(acceptedKeys, function(key) { if (key in data) result[key] = data[key]; });
+      return result;
+    },
+
+    // Define your own validations in here; call resolve() on deferred if you want to continue
+    validate: function(deferred) {
+      if (!this.isNew() && !this.a.selected_network_area_id) {
+        deferred.reject("A selected network area ID must be set.");
+      }
+      else {
+        deferred.resolve();
+      }
+    },
+  });
+
+  // Create a new prospect (with no data) to get a token, and then update it with a selected network area.
+  Prospect.create().then(function(prospect) {
+    console.log('created', prospect);
+    prospect.a.selected_network_area_id = 5;
+    return prospect.save();
+  }).done(function(prospect) {
+    console.log('now in state', prospect.attributes.state);
+  }).fail(function(obj, msg) {
+    console.log("fail", obj, msg);
+  });
+````
--- model.js
+++ model.js
@@ -1,173 +1,195 @@
 SpinalTap = {
   $: jQuery,
-  extend: function(object) {
-    _.each(object, function(value, key) { this.prototype[key] = value; }, this);
-  }
-};
 
-SpinalTap.Persistence = {
-  load: function(opts) {
-    opts = _.extend({dataType: "json"}, opts);
-    return SpinalTap.$.ajax(opts);
+  Persistence: {
+    load: function(opts) {
+      opts = _.extend({dataType: "json"}, opts);
+      return SpinalTap.$.ajax(opts);
+    },
+
+    save: function(opts) {
+      opts = _.extend({dataType: "json"}, opts);
+      return SpinalTap.$.ajax(opts);
+    }
   },
 
-  save: function(opts) {
-    opts = _.extend({dataType: "json"}, opts);
-    return SpinalTap.$.ajax(opts);
+  Model: function(object) {
+    this.recordClass = this.makeRecordClass();
+    _.extend(this, object);
   }
 };
 
-SpinalTap.Model = function(url) {
-  this.url = url;
-  this.recordClass = this.makeRecordClass();
-};
 
-SpinalTap.Model.prototype.makeRecordClass = function() {
-  var model = this;
-  var recordClass = function() {
-    var args = Array.prototype.slice(arguments);
-    SpinalTap.Record.apply(this, [model].concat(args));
-  };
+/* */
 
-  recordClass.extend = SpinalTap.extend;
-  recordClass.prototype = new SpinalTap.Record();
-  recordClass.prototype.constructor = recordClass;
-  return recordClass;
-};
 
-SpinalTap.Model.prototype.all = function(opts) {
-  var self = this;
+_.extend(SpinalTap.Model.prototype, {
+  extend: function(object) {
+    _.extend(this.recordClass.prototype, object);
+    return this;
+  },
 
-  opts = _.extend({url: this.url}, opts);
-  return SpinalTap.Persistence.load(opts).then(function(data) {
-    var attributesArray = self.recordClass.prototype.wireToAttributesArray(data);
-    return _.map(attributesArray, function(attributes) {
+  makeRecordClass: function() {
+    var model = this;
+    var recordClass = function() {
+      var args = Array.prototype.slice(arguments);
+      SpinalTap.Record.apply(this, [model].concat(args));
+    };
+
+    recordClass.prototype = new SpinalTap.Record();
+    recordClass.prototype.constructor = recordClass;
+    return recordClass;
+  },
+
+  all: function(opts) {
+    var self = this;
+
+    opts = _.extend({url: this.url}, opts);
+    return SpinalTap.Persistence.load(opts).then(function(data) {
+      var attributesArray = self.recordClass.prototype.wireToAttributesArray(data);
+      return _.map(attributesArray, function(attributes) {
+        return new self.recordClass(attributes, {persisted: true});
+      });
+    });
+  },
+
+  first: function(opts) {
+    var self = this;
+
+    opts = _.extend({url: this.url}, opts);
+    return SpinalTap.Persistence.load(opts).then(function(data) {
+      var attributes = self.recordClass.prototype.wireToAttributes(data);
       return new self.recordClass(attributes, {persisted: true});
     });
-  });
-};
+  },
 
-SpinalTap.Model.prototype.first = function(opts) {
-  var self = this;
+  find: function(id, opts) {
+    return this.first(_.extend({url: this.url + "/" + id}, opts));
+  },
 
-  opts = _.extend({url: this.url}, opts);
-  console.log('first', opts);
-  return SpinalTap.Persistence.load(opts).then(function(data) {
-    var attributes = self.recordClass.prototype.wireToAttributes(data);
-    return new self.recordClass(attributes, {persisted: true});
-  });
-};
-
-SpinalTap.Model.prototype.find = function(id, opts) {
-  return this.first(_.extend({url: this.url + "/" + id}, opts));
-};
-
-SpinalTap.Model.prototype.create = function(attributes) {
-  var record = new this.recordClass(attributes);
-  return record.save();
-};
+  create: function(attributes) {
+    var record = new this.recordClass(attributes);
+    return record.save();
+  }
+});
 
 /* */
 
 SpinalTap.Record = function(model, attributes, opts) {
-  console.log("new", model, attributes, opts);
+  if (!model) return; // we get called with no arguments as part of cloning
+
   this.model = model;
-  this.attributes = attributes === void 0 ? {} : attributes;
+  this.eventSink = SpinalTap.$(this);
   this.opts = opts === void 0 ? {} : opts;
-  this.persistedAttributes = this.opts.persisted ? _.extend({}, this.attributes) : {};
-  this.initialize(); // TODO : fire initialize event instead?
+
+  this.registerEvents();
+  this.eventSink.trigger("initialization");
+  this.setAttributes(attributes, {reset: true, persisted: this.opts.persisted});
 };
 
-SpinalTap.Record.prototype.getID = function() {
-  return this.attributes.id;
-};
+_.extend(SpinalTap.Record.prototype, {
+  events: {},
 
-SpinalTap.Record.prototype.getURL = function() {
-  return this.getID() ? this.model.url + "/" + encodeURIComponent(this.getID()) : this.model.url;
-};
+  registerEvents: function() {
+    _.each(this.events, function(object, eventName) { this.eventSink.on(eventName, object); }, this);
+  },
 
-SpinalTap.Record.prototype.isNewRecord = function() {
-  return !this.getID();
-};
+  getID: function() {
+    return this.attributes.id;
+  },
 
-SpinalTap.Record.prototype.getChangedAttributes = function() {
-  var changed = {}, attribs = this.attributes, persisted = this.persistedAttributes;
+  getURL: function() {
+    return this.isNew() ? this.model.url : this.model.url + "/" + encodeURIComponent(this.getID());
+  },
 
-  _.each(attribs, function(value, key) { if (value !== persisted[key]) changed[key] = value; });
+  isNew: function() {
+    return !this.getID();
+  },
 
-  return changed;
-};
+  getChangedAttributes: function() {
+    var changed = {}, attribs = this.attributes, persisted = this.persistedAttributes;
 
-SpinalTap.Record.prototype.getSaveableAttributes = function() {
-  return this.getChangedAttributes();
-};
+    _.each(attribs, function(value, key) { if (value !== persisted[key]) changed[key] = value; });
 
-SpinalTap.Record.prototype.initialize = function() { };
+    return changed;
+  },
 
-SpinalTap.Record.prototype.reload = function(opts) {
-  var self = this;
-  opts = _.extend({url: this.getURL()}, opts);
-  return SpinalTap.Persistence.load(opts).then(function(data) {
-    var attributes = self.wireToAttributes(data);
-    self.setAttributes(attributes, {persisted: true});
-  });
-};
+  getSaveableAttributes: function() {
+    return this.getChangedAttributes();
+  },
 
-SpinalTap.Record.prototype.save = function(opts) {
-  var self = this;
+  reload: function(opts) {
+    var self = this;
+    opts = _.extend({url: this.getURL()}, opts);
+    return SpinalTap.Persistence.load(opts).then(function(data) {
+      var attributes = self.wireToAttributes(data);
+      self.setAttributes(attributes, {persisted: true});
+    });
+  },
 
-  opts = _.extend({
-    url:    this.getURL(),
-    method: this.isNewRecord() ? "POST" : "PUT",
-    data:   this.attributesToWire(this.getSaveableAttributes()),
-  }, opts);
+  validate: function(deferred) {
+    deferred.resolve();
+  },
 
-  console.log(".save", opts);
+  save: function(opts) {
+    var validator = SpinalTap.$.Deferred();
 
-  return SpinalTap.Persistence.save(opts).then(function(data) {
-    self.processSaveResults(data);
-    return self;
-  });
-};
+    this.validate(validator);
 
-SpinalTap.Record.prototype.processSaveResults = function(data) {
-  console.log("save returned", data);
-  var attributes = this.wireToAttributes(data);
-  this.setAttributes(attributes, {persisted: true, reset: true});
-}
+    return validator.then(_.bind(this.saveWithoutValidation, this, opts));
+  },
 
-SpinalTap.Record.prototype.setAttributes = function(attributes, opts) {
-  if (opts && opts.reset) {
-    this.attributes = {};
-    this.persistedAttributes = {};
-  }
-  _.extend(this.attributes, attributes);
-  if (opts && opts.persisted) {
-    _.extend(this.persistedAttributes, attributes);
-  }
-};
+  saveWithoutValidation: function(opts) {
+    opts = _.extend({
+      url:    this.getURL(),
+      method: this.isNew() ? "POST" : "PUT",
+      data:   this.attributesToWire(this.getSaveableAttributes()),
+    }, opts);
 
-SpinalTap.Record.prototype.updateAttributes = function(attributes, opts) {
-  this.setAttributes(attributes, opts);
-  return this.save();
-};
+    this.eventSink.trigger("beforeSave", opts);
 
-/*
- * Converting between attributes hashes and what we send/receive on the wire
- *
- */
+    return SpinalTap.Persistence.save(opts).then(_.bind(this.processSaveResults, this));
+  },
 
-SpinalTap.Record.prototype.wireToAttributesArray = function(wireData) {
-  var self = this;
-  return _.map(wireData, function(object) { return self.wireToAttributes(object); });
-};
+  processSaveResults: function(data) {
+    var attributes = this.wireToAttributes(data);
+    this.setAttributes(attributes, {persisted: true, reset: true});
+    this.eventSink.trigger("afterSave", data);
+    return this;
+  },
 
-SpinalTap.Record.prototype.wireToAttributes = function(wireData) {
-  return wireData;
-};
+  setAttributes: function(attributes, opts) {
+    if (opts && opts.reset) {
+      this.attributes = this.a = {};
+      this.persistedAttributes = {};
+    }
 
-SpinalTap.Record.prototype.attributesToWire = function(attributes) {
-  return attributes;
-};
+    if (attributes !== void 0) {
+      _.extend(this.attributes, attributes);
+      if (opts && opts.persisted) {
+        _.extend(this.persistedAttributes, attributes);
+      }
+    }
+  },
 
-// TODO : destroy, validations, events
+  updateAttributes: function(attributes, opts) {
+    this.setAttributes(attributes, opts);
+    return this.save();
+  },
+
+  /* Converting between attributes hashes and what we send/receive on the wire */
+
+  wireToAttributesArray: function(wireData) {
+    return _.map(wireData, this.wireToAttributes, this);
+  },
+
+  wireToAttributes: function(wireData) {
+    return wireData;
+  },
+
+  attributesToWire: function(attributes) {
+    return attributes;
+  },
+
+  // TODO : destroy
+});
