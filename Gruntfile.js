6f2ec64529fd526fab9e7c8c3cafcc5782e74be3
PrototypeAlex <darth.gibson@gmail.com>
2013-12-19 16:11:47 +1300

    Added concat and uglify tasks to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,44 @@ module.exports = function(grunt) {
         '// Distributed under MIT license\n' +
         '//\n' + 
         '// https://github.com/mogest/spinal-tap\n' +
-        '\n',
+        '\n'
+    },
 
+    concat: {
+      options: {
+        banner: "<%= meta.banner %>"
+      },
+      build: {
+        src: [
+               'src/spinal-tap.js',
+               'src/model.js',
+               'src/record.js',
+               'src/persistence.js'
+             ],
+        dest: 'dist/spinal-tap.js'
+      }
+    },
+
+    uglify : {
+      options: {
+        banner: "<%= meta.banner %>"
+      },
+
+      build : {
+        src : 'dist/spinal-tap.js',
+        dest : 'dist/spinal-tap.min.js',
+        options : {
+          sourceMap : 'dist/spinal-tap.map',
+          sourceMappingURL : 'spinal-tap.map'
+        }
+      }
     }
 
-    
-
   });
+
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
 }
